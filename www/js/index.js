5385223960443fae2f4e669d65666398c5d46f1d
bethleharaja <raja.bethleham@mobigesture.com>
2013-06-26 15:56:03 +0530

    only the testing file

--- www/js/index.js
+++ www/js/index.js
@@ -26,12 +26,8 @@ var app = {
         // Bind any events that are required on startup. Common events are:
         // 'load', 'deviceready', 'offline', and 'online'.
         bindEvents: function() {
-            document.addEventListener('deviceready', app.onDeviceReady, false);
+            document.addEventListener('deviceready', this.onDeviceReady, false);
         },
-        // deviceready Event Handler
-        //
-        // The scope of 'this' is the event. In order to call the 'receivedEvent'
-        // function, we must explicity call 'app.receivedEvent(...);'
         onDeviceReady: function() {
             navigator.accelerometer.getCurrentAccleration(app.onSuccess, app.onError);
             alert("accelerometer called");
